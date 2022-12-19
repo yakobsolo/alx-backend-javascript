6756fe130ae3cfa8a0f2fa46a95119a0295d427a
yakobsolo <yakisolo2009@gmail.com>
2022-12-19 11:08:03 +0000

    block scope

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -4,8 +4,8 @@ export default function taskBlock(trueOrFalse) {
 
   if (trueOrFalse) {
     /* BLOCK SCOPE */
-    let task = true;
-    let task2 = false;
+    const task = true;
+    const task2 = false;
   }
 
   return [task, task2];
