e14f1c6f93369b32e67df625a786a7e653e31d22
yakobsolo <yakisolo2009@gmail.com>
2022-12-19 15:03:19 +0000

    restparameter

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,7 +3,7 @@ export default function taskBlock(trueOrFalse) {
   const task2 = true;
 
   if (trueOrFalse) {
-    /* block-scope */
+    /* blockscope */
     const task = true;
     const task2 = false;
   }
