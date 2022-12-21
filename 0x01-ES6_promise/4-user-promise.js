20fd5826d718cb2231a9b78d7f5314231663fa9b
yakobsolo <yakisolo2009@gmail.com>
2022-12-21 08:45:15 +0000

    user promise

---
+++ 0x01-ES6_promise/4-user-promise.js
@@ -0,0 +1,9 @@
+export default function signUpUser(firstName, lastName) {
+  return new Promise((resolve) => {
+    resolve({
+      "firstName": firstName,
+      "lastName": lastName,
+    });
+  });
+}
+
