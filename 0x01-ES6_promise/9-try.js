b6aa729ccca061eb448c1a3762d5b234e3e85b8d
yakobsolo <yakisolo2009@gmail.com>
2022-12-21 16:54:46 +0000

    await

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,6 +1,12 @@
 export default function guardrail(mathFunction) {
 const queue = [];
-const val = await mathFunction();
+const val;
+try {
+  val = mathFunction();
+} catch(err) {
+  val = err.toString();
+}
 arr.push(val);
 arr.push("Guardrail was processed");
+return queue;
 }
