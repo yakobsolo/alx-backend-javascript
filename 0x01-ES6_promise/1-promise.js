5d31c3f8ea63e69f2ff14b73697426c8c33812dc
yakobsolo <yakisolo2009@gmail.com>
2022-12-21 08:04:56 +0000

    promis 1

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,6 +1,6 @@
 export function getFullResponseFromAPI(success) {
   return new Promise((resolve, reject) => {
-    if(success) {
+    if(success === true) {
       resolve({
         status: 200,
         body: 'Success',
