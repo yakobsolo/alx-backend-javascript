f13d052e28df926ce195208b409671160c6cc6ad
yakobsolo <yakisolo2009@gmail.com>
2022-12-21 08:07:06 +0000

    promis 1

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,12 +1,12 @@
-export function getFullResponseFromAPI(success) {
+export default function getFullResponseFromAPI(success) {
   return new Promise((resolve, reject) => {
-    if(success === true) {
+    if (success === true) {
       resolve({
         status: 200,
         body: 'Success',
       });
     } else {
-      reject(new Error("The fake API is not working currently"));
+      reject(new Error('The fake API is not working currently'));
     }
   });
 }
