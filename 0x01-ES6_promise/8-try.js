b73136d89ba8c115040cb43a2e127767a6a97c53
yakobsolo <yakisolo2009@gmail.com>
2022-12-21 16:18:08 +0000

    throw try catch

--- 0x01-ES6_promise/8-try.js
+++ 0x01-ES6_promise/8-try.js
@@ -1,9 +1,7 @@
 export default function divideFunction(numerator, denominator) {
-  try {
-    if(denominator === 0) {
-      throw new Error("cannot divide by 0");
-    } else {
+  if (denominator === 0) {
+    throw new Error("cannot divide by 0");
+  } else {
       return numerator / denominator;
     }
-  }
 }
