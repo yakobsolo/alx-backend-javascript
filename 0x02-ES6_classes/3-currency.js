f1dad745d392ee5e689dab61482f6db603d35abc
yakobsolo <yakisolo2009@gmail.com>
2022-12-22 10:19:28 +0000

    setter:getter:return function

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -5,21 +5,26 @@ export default class Currency {
     this._code = code;
     this._name = name;
   }
+
   get code() {
     return this._code;
   }
+
   get name() {
-    return this.name;
+    return this._name;
   }
+
   set code(codval) {
     if (typeof codval !== 'string') throw TypeError('name must be a string');
-    this._code = codval
+    this._code = codval;
   }
+
   set name(nameval) {
     if (typeof nameval !== 'string') throw TypeError('name must be a string');	  
     this._name = nameval;
   }
-  function displayFullCurrency() {
+
+  displayFullCurrency() {
     return `${this._name} (${this._code})`;
   }
 }
