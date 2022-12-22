9bc46357b995be3ae4d8ec1f274dce406cee97b6
yakobsolo <yakisolo2009@gmail.com>
2022-12-22 09:05:44 +0000

    returning class object

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -2,5 +2,5 @@ import ClassRoom from './0-classroom'
 
 
 export default function initializeRooms() {
-  return [ClassRoom(19),ClassRoom(20),ClassRoom(34)];
+  return [new ClassRoom(19),new ClassRoom(20),new ClassRoom(34)];
 }
