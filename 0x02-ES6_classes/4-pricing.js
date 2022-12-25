754b03ed7c2b4f354e94f50a2452c928b51f5ff7
yakobsolo <yakisolo2009@gmail.com>
2022-12-25 06:20:08 +0000

    pricing

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,30 +1,29 @@
-import Currency from './3-currency'
-
+import Currency from './3-currency';
 
 export default class Pricing {
   constructor(amount, currency) {
-    this._amount = amount;
-    this._currency = currency;
+    this.amount = amount;
+    this.currency = currency;
   }
-  
+
   get amount() {
     return this._amount;
-  }
-   
-  get currency() {
-    return this._currency;
   }
 
   set amount(newamount) {
     this._amount = newamount;
   }
 
+  get currency() {
+    return this._currency;
+  }
+
   set currency(newcurrency) {
     this._currency = newcurrency;
   }
-  
+
   displayFullPrice() {
-    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
+    return `${this.amount} ${this.currency.name} (${this.currency.code})`;
   }
 
   static convertPrice(amount, conversionRate) {
