67dda7d9eaeaa1c69d309efa88a82f65d0207fec
yakobsolo <yakisolo2009@gmail.com>
2023-02-22 16:53:49 +0000

    updating

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+""" basic annotation add 
+Return: return add of a and b
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    a: float
+    b: float
+    """
+    return a + b
