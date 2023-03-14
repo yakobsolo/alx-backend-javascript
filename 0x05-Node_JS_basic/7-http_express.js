a7aa453f615804ade60d38ebfab3ec91d0a765e9
yakobsolo <yakisolo2009@gmail.com>
2023-03-14 16:43:54 +0000

    http exress

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -1,16 +1,29 @@
 const express = require('express');
+
+const args = process.argv.slice(2);
 const countStudents = require('./3-read_file_async');
 
+const DATABASE = args[0];
+
 const app = express();
-app.get('/', (req, res) => res.send('Hello Holberton School!'));
+const port = 1245;
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
 app.get('/students', async (req, res) => {
-  const title = 'This is the list of our students\n';
+  const msg = 'This is the list of our students\n';
   try {
-    const data = await countStudents(process.argv[2]);
-    res.send(`${title}${data.join('\n')}`);
+    const students = await countStudents(DATABASE);
+    res.send(`${msg}${students.join('\n')}`);
   } catch (error) {
-    res.send(`${title}${error.message}`);
+    res.send(`${msg}${error.message}`);
   }
 });
-app.listen(1245);
+
+app.listen(port, () => {
+  //   console.log(`Example app listening at http://localhost:${port}`);
+});
+
 module.exports = app;
